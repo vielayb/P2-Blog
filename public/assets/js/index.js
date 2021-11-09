ff1f547e4122e3d10286306f06dea938528cc29f
Jake <jake.fay9673@gmail.com>
2021-11-09 14:06:28 -0500

    pages setup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,48 +1,12 @@
-
-//Widget for uploading pictures
-function showUploadWidget() { cloudinary.openUploadWidget({    
-    cloudName: "<dom1gkn9c>",    
-    uploadPreset: "<ml_default>",    
-    sources: [
-    "local",
-    "google_drive",
-    "dropbox",
-    "camera",
-    "image_search",
-    "instagram",
-    "facebook"
-    ],
-    googleApiKey: "<image_search_google_api_key>",
-        showAdvancedOptions: true,
-        cropping: true,
-        multiple: false,
-        defaultSource: "local",
-        styles: {
-            palette: {
-                window: "#FFFFFF",
-                windowBorder: "#90A0B3",
-                tabIcon: "#0078FF",
-                menuIcons: "#5A616A",
-                textDark: "#000000",
-                textLight: "#FFFFFF",
-                link: "#0078FF",
-                action: "#FF5A00",
-                inactiveTabIcon: "#0E2F5A",
-                error: "#F44235",
-                inProgress: "#0078FF",
-                complete: "#20B832",
-                sourceBg: "#E4EBF1"
-            },
-            fonts: { 
-                default: {
-                    active: true
-                }
-            }
+const widgetBtn = document.querySelector("#upload_widget");
+widgetBtn.addEventListener('click', function () {
+    window.cloudinary.createUploadWidget({
+        cloudName: 'dom1gkn9c',
+        uploadPreset: 'ml_default'
+    }, (error, result) => {
+        if (!error && result && result.event === "success") {
+            console.log('Done! Here is the image info: ', result.info);
         }
-    }, 
-    (err, info) => {
-        if (!err) {
-            console.log("Upload Widget event - ", info);
-        }  
-    }); 
-}
+    }
+    ) .open();
+})
