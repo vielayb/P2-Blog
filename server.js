a7103475665e4c5ec8bea82e9293b3ac20a91300
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2021-11-08 20:08:46 -0500

    commits

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(express.static("public"))
+
+
+app.listen(PORT, ()=> {
+    console.log("http://localhost:8080")
+})
