0a1b794d8a839094a813dc952104ef28eaf84ef1
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2021-11-14 21:29:43 -0500

    Please Work

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,43 +1,26 @@
-let image; 
 async function newFormHandler(event) {
     event.preventDefault();
-
+  
     const title = document.querySelector('input[name="post-title"]').value;
     const post_url = document.querySelector('input[name="post-url"]').value;
-
+  
     const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            post_url,
-            image
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_url
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-
+  
     if (response.ok) {
-        document.location.replace('/dashboard');
+      document.location.replace('/dashboard');
     } else {
-        alert(response.statusText);
+      alert(response.statusText);
     }
-}
-
-const widgetBtn = document.querySelector("#upload_widget");
-widgetBtn.addEventListener('click', function () {
-    window.cloudinary.createUploadWidget({
-        cloudName: 'dom1gkn9c',
-        uploadPreset: 'ml_default'
-    }, (error, result) => {
-        if (!error && result && result.event === "success") {
-            console.log('Done! Here is the image info: ', result.info);
-            console.log(result.info.public_id);
-            document.querySelector('#upLoad').src = result.info.url;
-            image = result.info.url;
-        }
-    }
-    ) .open();
-})
-
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
